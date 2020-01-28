8cd8eb76e1c1eeafd035795d3f27b51e90637bd4
Nathan Oliveira <52627147+nathan-oliveira@users.noreply.github.com>
2020-01-28 18:22:09 -0300

    inicio do curso

---
+++ execucao-js/js/js.js
@@ -0,0 +1,27 @@
+//declaração de variáveis
+var nome = "Nathan";
+var sobreNome = "Oliveira Mendonça";
+
+console.log("Instrução solta em um arquivo JS");
+
+//declaração de função
+function exibirNomeConsole(){
+    console.log("exibir nome: " + nome);
+}
+
+function exibirNomeCompleto(){
+    console.log("Professor, nome completo: " + nome + " " + sobreNome);
+}
+
+function eventoClick(){
+    alert("Você clicou em um botão!");
+}
+
+//Chamadas de função
+exibirNomeConsole();
+
+
+
+
+
+
---
+++ exemplo-programacao-OO-e-funcional-em-JS/js/js.js
@@ -0,0 +1,32 @@
+//funcional
+function sejaBemVindo(){
+    console.log("Seja bem vindo a Formação JavaScript Mestre Jedi!!!");
+}
+
+console.log("Chamando a função seja bem vindo sejaBemVindo():");
+sejaBemVindo();
+
+//POO
+var obj = {
+    nome: "Nathan Oliveira",
+    curso: "Formação JavaScript Mestre Jedi!!! :)",
+    mistrarAula: function(){
+        console.log("Ministrando aula de JavaScript!");
+    }
+};
+
+console.log("objeto");
+console.log(obj);
+
+console.log("acessando propriedades do objeto: obj.nome");
+console.log(obj.nome);
+console.log(obj.curso);
+
+console.log("chamando o método do objeto professor: obj.mistrarAula()");
+objProfessor.mistrarAula();
+
+
+
+
+
+
