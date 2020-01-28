4fb7827eff277af66f7121314dc5e80ec8bdc0fd
Nathan Oliveira <52627147+nathan-oliveira@users.noreply.github.com>
2020-01-28 20:46:53 -0300

    Seção 8

---
+++ tipos-de-dados/js/string.js
@@ -0,0 +1,22 @@
+var nome = "Nathan";
+var sobreNome = 'Oliveira Mendonça';
+var nomeCompleto = nome + " " + sobreNome;
+var vazia = "";
+var texto = "texto \
+texto \
+texto";
+
+console.log("Nome completo: " + nomeCompleto + vazia);
+
+var numeroString = "20" + 10;
+var numeroStringInv = 10 + "50";
+
+console.log("valor: " + numeroString + " - " + numeroStringInv);
+
+var stringVar = "string texto";
+var numeroVar = 10;
+
+console.log("Tipo1:" + typeof stringVar);
+console.log("Tipo2:" + typeof numeroVar);
+
+console.log("Tamanho string:" + stringVar.length);
